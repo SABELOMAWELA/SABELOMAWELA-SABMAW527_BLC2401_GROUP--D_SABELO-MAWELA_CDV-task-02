4b0ed5091d0d083436d3363e7dd3c25c29a83460
SABELOMAWELA <Mawelas63@gmail.com>
2024-06-28 14:50:37 +0200

    "Added special forms for if, while, do, and define, implemented function definition and application, and included example runs."

--- index2.js
+++ index2.js
@@ -72,3 +72,96 @@ function parseExpression(program) {
     }
   }
   
+  const specialForms = Object.create(null);
+  
+  specialForms.if = (args, scope) => {
+    if (args.length != 3) {
+      throw new SyntaxError("Bad number of args to if");
+    }
+    if (evaluate(args[0], scope) !== false) {
+      return evaluate(args[1], scope);
+    } else {
+      return evaluate(args[2], scope);
+    }
+  };
+  
+  specialForms.while = (args, scope) => {
+    if (args.length != 2) {
+      throw new SyntaxError("Bad number of args to while");
+    }
+    while (evaluate(args[0], scope) !== false) {
+      evaluate(args[1], scope);
+    }
+    return false;
+  };
+  
+  specialForms.do = (args, scope) => {
+    let value = false;
+    for (let arg of args) {
+      value = evaluate(arg, scope);
+    }
+    return value;
+  };
+  
+  specialForms.define = (args, scope) => {
+    if (args.length != 2 || args[0].type != "word") {
+      throw new SyntaxError("Bad use of define");
+    }
+    let value = evaluate(args[1], scope);
+    scope[args[0].name] = value;
+    return value;
+  };
+  
+  const topScope = Object.create(null);
+  
+  topScope.true = true;
+  topScope.false = false;
+  
+  ["+", "-", "*", "/", "==", "<", ">"].forEach(op => {
+    topScope[op] = Function("a, b", return a ${op} b;);
+  });
+  
+  topScope.print = value => {
+    console.log(value);
+    return value;
+  };
+  
+  function run(program) {
+    return evaluate(parse(program), Object.create(topScope));
+  }
+  
+  run(`
+    do(define(x, 10),
+       if(>(x, 5),
+          print("large"),
+          print("small")))
+  `);
+  
+  specialForms.fun = (args, scope) => {
+    if (!args.length) {
+      throw new SyntaxError("Functions need a body");
+    }
+    let body = args[args.length - 1];
+    let params = args.slice(0, args.length - 1).map(expr => {
+      if (expr.type != "word") {
+        throw new SyntaxError("Parameter names must be words");
+      }
+      return expr.name;
+    });
+  
+    return function() {
+      if (arguments.length != params.length) {
+        throw new TypeError("Wrong number of arguments");
+      }
+      let localScope = Object.create(scope);
+      for (let i = 0; i < arguments.length; i++) {
+        localScope[params[i]] = arguments[i];
+      }
+      return evaluate(body, localScope);
+    };
+  };
+  
+  run(`
+    do(define(f, fun(a, b, +(a, b))),
+       print(f(4, 5)))
+  `);
